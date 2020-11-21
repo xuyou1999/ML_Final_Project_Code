a69683c5a7675b4b2ab81d6256b4019482951c1b
JunchengDong0421 <jd4115@nyu.edu>
2020-11-21 21:57:29 +0800

    Update 1.1

--- movie_info_demo.py
+++ movie_info_demo.py
@@ -1,44 +1,61 @@
 from requests_html import HTMLSession
 
 s = HTMLSession()
-data = s.get("https://movie.douban.com/subject/30220799/")  # 冷门：6828945  热门：30220799
+
+ID = "30220799"  # 冷门：6828945  热门：30220799
+data = s.get("https://movie.douban.com/subject/" + ID + "/")
 info = data.html.find("div#info", first=True)
 info_text = info.text.split("\n")
 info_list = [i.split(": ") for i in info_text]
 info_dict = {i[0]: i[1].split(" / ") for i in info_list}
-# attr_list = info.find("span.attrs")
-# director = attr_list[0]
-# director_name = director.text.split(" / ")
+attr_list = info.find("span.attrs")
+
+director_index = 0
+screenwriter_index = 1
 actor_index = 2
+
 try:
     director_name = info_dict["导演"]
+    director = attr_list[director_index]
+    director_attr = director.find("a")
+    director_href = [d.attrs["href"] for d in director_attr]
 except KeyError:
     director_name = None
+    director_href = None
+    screenwriter_index -= 1
     actor_index -= 1
+
 try:
     screenwriter_name = info_dict["编剧"]
+    screenwriter = attr_list[screenwriter_index]
+    screenwriter_attr = screenwriter.find("a")
+    screenwriter_href = [s.attrs["href"] for s in screenwriter_attr]
 except KeyError:
     screenwriter_name = None
+    screenwriter_href = None
     actor_index -= 1
-print("Director:", director_name)
-# screenwriter = attr_list[1]
-# screenwriter_name = screenwriter.text.split(" / ")
-print("Screenwriter:", screenwriter_name)
 
+print("Director Name:", director_name)
+print("Director Href:", director_href)
 
-# actor_name = actor.text.split(" / ")
+print("Screenwriter Name:", screenwriter_name)
+print("Screenwriter Href:", screenwriter_href)
+
 try:
     actor_name = info_dict["主演"]
-    attr_list = info.find("span.attrs")
     actor = attr_list[actor_index]
     actor_attr = actor.find("a")
     actor_href = [a.attrs["href"] for a in actor_attr]
     actor_url = ["movie.douban.com" + href for href in actor_href]
 except KeyError:
     actor_name = None
+    actor_href = None
+    actor_url = None
+
+print("Actor Names:", actor_name)
 print("Actor Href:", actor_href)
 print("Actor URL:", actor_url)
-print("Actors:", actor_name)
+
 
 rate = data.html.find("div.rating_self", first=True)
 rating_info = rate.text.split("\n")
--- search_demo.py
+++ search_demo.py
@@ -1,6 +1,46 @@
-from requests_html import HTMLSession
+import asyncio
+from pyppeteer import launch
+from lxml import etree
 
-s = HTMLSession()
+key_word = "indo"
+opt = {
+    "ignoreHTTPSErrors": True,
+    "headless": False,
+    'userDataDir': r'D:\temp',  # path for temporary data
+    "args": ['--no-sandbox', "--disable-infobars"]
+}
 
-key_word = "爱"
-search_data = s.get("https://search.douban.com/movie/subject_search?search_text=" + key_word + "&cat=1002")
+
+def process(cont):
+    document = etree.HTML(cont)
+    source_list = document.xpath("//a")
+    celeb_file = open("celeb_data.txt", 'a')
+    movie_file = open("movie_data.txt", 'a')
+    celeb_file.write(key_word + "\n")
+    movie_file.write(key_word + "\n")
+    for s in source_list:
+        link = s.attrib["href"]
+        if "movie.douban.com/celebrity" in link:
+            celeb_file.write(link.split("/")[-2] + "\n")
+        elif "movie.douban.com/subject" in link:
+            movie_file.write(link.split("/")[-2] + "\n")
+
+
+async def main(url):
+    browser = await launch(opt)
+    page = await browser.newPage()
+    await page.setJavaScriptEnabled(enabled=True)
+    await page.goto(url, {"waitUntil": "networkidle2"})
+    content = await page.content()
+    process(content)
+    await page.close()
+
+
+if __name__ == '__main__':
+    start = 0
+    url = "https://search.douban.com/movie/subject_search?search_text=" + key_word + "&cat=1002&start=" + str(start)
+    try:
+        asyncio.get_event_loop().run_until_complete(main(url))
+    except Exception as e:
+        print(e)
+
