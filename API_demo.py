5898a041a999e1ab862706d797a4ea4f86354d1f
xuyou1999 <yx1711@nyu.edu>
2020-11-22 16:26:14 +0800

    you xu commit

--- API_demo.py
+++ API_demo.py
@@ -9,11 +9,11 @@ if __name__ == '__main__':
     for id in [4864908, 26607693, 26662193, 6828945]:
         d.search(id)
         info = d.info()
-        print(info)
+        # print(info)
         data = {"director": [info["director_id"]], "screenwriter": [info["screenwriter_id"]],
                 "actor": [info["actor_id"]], "genre": [info["类型"]], "region": [info["制片国家/地区"]],
                 "date": [info["上映日期"]], "rating": [info["rating"]], "rating_count": [info["rating_count"]],
                 "rating_weight": [info["rating_weight"]]}
         new = pd.DataFrame(data, columns=columns)
         df = pd.concat([df, new], ignore_index=True)
-    print(df["screenwriter"])
+    print(df)
