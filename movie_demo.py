078dc22d614c7539487ef2863a77b53d7cfe4dc0
JunchengDong0421 <jd4115@nyu.edu>
2020-11-29 00:57:30 +0800

    movie id data

--- movie_demo.py
+++ movie_demo.py
@@ -8,20 +8,16 @@ ip_pool = [
     '101.86.86.101:8118',
 ]
 proxy_pool = [ip + 'http://' for ip in ip_pool]
-start = 0
+start = 3180  # 每次加60
 proxy_num = 0
-proxies = {'http':proxy_pool[proxy_num]}
+proxy = {'http': "http://112.111.217.36:9999"}
 while True:
     url = "https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=&start=" + str(start) + \
           "&year_range=2010,2019"
     headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                              "Chrome/60.0.3100.0 Safari/537.36"}
-    response = s.get(url, headers=headers, proxies=proxies)
-    try:
-        data = json.loads(response.text)["data"]
-    except KeyError:
-        proxy_num += 1
-        continue
+    response = s.get(url, headers=headers, proxies=proxy)
+    data = json.loads(response.text)["data"]
     for movie in data:
         id = movie["id"]
         with open("movie.txt", 'a') as file:
