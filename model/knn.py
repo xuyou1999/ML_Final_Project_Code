fd267f16f640766ac8ff2241b6416dec7c0f37ee
JunchengDong0421 <jd4115@nyu.edu>
2020-12-01 10:34:30 +0800

    example on how to load data

--- model/knn.py
+++ model/knn.py
@@ -3,3 +3,6 @@ from sklearn.neighbors import KNeighborsRegressor
 
 train = pd.read_csv("../data/train_data.csv")
 test = pd.read_csv("../data/test_data.csv")
+X_train, y_train, X_test, y_test = train.iloc[:, :-1], train.iloc[:, -1], test.iloc[:, :-1], test.iloc[:, -1]
+# print(X_train)
+# print(y_train)
