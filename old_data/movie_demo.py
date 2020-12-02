43c6ace55ed87fda7e16d7ac835d05ba077b3f1b
xuyou1999 <yx1711@nyu.edu>
2020-12-02 23:44:12 +0800

    crawl

--- old_data/movie_demo.py
+++ old_data/movie_demo.py
@@ -1,4 +1,5 @@
 import json
+import time
 from requests import Session
 import os
 
@@ -13,7 +14,7 @@ ip_pool = [
     '101.86.86.101:8118',
 ]
 proxy_pool = [ip + 'http://' for ip in ip_pool]
-start = 8100  # 每次加60
+start = 9980  # 每次加60
 proxy_num = 0
 proxy = {'http': "http://113.120.143.234:13456"}
 while True:
