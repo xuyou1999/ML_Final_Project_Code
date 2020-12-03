8e4f75267042c30c371d94f988868fabd4b0c531
JunchengDong0421 <jd4115@nyu.edu>
2020-12-03 15:34:46 +0800

    non-random

--- model/tree.py
+++ model/tree.py
@@ -12,10 +12,11 @@ os.chdir(path)
 train = pd.read_csv("../data/train_data.csv")
 test = pd.read_csv("../data/test_data.csv")
 X_train, y_train, X_test, y_test = train.iloc[:, 1:-1], train.iloc[:, -1], test.iloc[:, 1:-1], test.iloc[:, -1]
-model_tree = tree.DecisionTreeRegressor(max_depth=None)
-model_tree = model_tree.fit(X_train, y_train)
-y_pred_tree = model_tree.predict(X_test)
-print(mean_squared_error(y_test, y_pred_tree))
+for i in range(1, 7):
+    model_tree = tree.DecisionTreeRegressor(max_depth=None, max_features=i, random_state=1)
+    model_tree = model_tree.fit(X_train, y_train)
+    y_pred_tree = model_tree.predict(X_test)
+    print(mean_squared_error(y_test, y_pred_tree))
 
 
 # Bagging
