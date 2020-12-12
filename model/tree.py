5751c3cff7506f8263ac5a477c79ebe9289574e9
xuyou1999 <yx1711@nyu.edu>
2020-12-12 18:32:12 +0800

    random forest

--- model/tree.py
+++ model/tree.py
@@ -4,22 +4,39 @@ import pandas as pd
 from sklearn import tree
 from sklearn.ensemble import BaggingRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+
 
 abs_path = os.path.abspath(__file__)
 path = os.path.dirname(abs_path)
 os.chdir(path)
 
 train = pd.read_csv("../data/train_data.csv")
+validate = pd.read_csv("../data/validate_data.csv")
 test = pd.read_csv("../data/test_data.csv")
-X_train, y_train, X_test, y_test = train.iloc[:, 1:-1], train.iloc[:, -1], test.iloc[:, 1:-1], test.iloc[:, -1]
-for i in range(1, 7):
-    model_tree = tree.DecisionTreeRegressor(max_depth=None, max_features=i, random_state=1)
-    model_tree = model_tree.fit(X_train, y_train)
-    y_pred_tree = model_tree.predict(X_test)
-    print(mean_squared_error(y_test, y_pred_tree))
+
+X_train, y_train, X_validate, y_validate, X_test, y_test = train.iloc[:, 1:-1], train.iloc[:, -1], \
+                                validate.iloc[:, 1:-1], validate.iloc[:, -1], test.iloc[:, 1:-1], test.iloc[:, -1]
+
+# tree with not limitation on max_features
+model_tree = tree.DecisionTreeRegressor(max_depth=None, random_state=1)
+model_tree = model_tree.fit(X_train, y_train)
+y_pred_tree = model_tree.predict(X_test)
+print('Tree error', mean_squared_error(y_test, y_pred_tree))
 
 
-# Bagging
-model_bag = BaggingRegressor(base_estimator=tree.DecisionTreeRegressor(max_depth=None), n_estimators=20).fit(X_train, y_train)
-y_pred_bag = model_bag.predict(X_test)
-print(mean_squared_error(y_test, y_pred_bag))
+# Random forest by setting the number of trees using val set, max_features is by deault as sqrt
+opt_estimators = 1
+opt_error = 100
+for i in range(1, 200):
+    model_random_forest = RandomForestRegressor(random_state=1, n_estimators=i).fit(X_train, y_train)
+    y_pred_random_forest = model_random_forest.predict(X_validate)
+    error = mean_squared_error(y_validate, y_pred_random_forest)
+    if error < opt_error:
+        opt_error = error
+        opt_estimators = i
+    print('Ramdom forest error:', error)
+
+print(opt_error) # 0.09151789030831993
+print(opt_estimators) # 159
+
