a164c5facf0c8d2481a40dab11623c9c42fedfcb
JunchengDong0421 <jd4115@nyu.edu>
2020-12-13 15:12:31 +0800

    nn

--- model/nn.py
+++ model/nn.py
@@ -15,24 +15,29 @@ validate = pd.read_csv("../data/validate_data.csv")
 test = pd.read_csv("../data/test_data.csv")
 
 X_train, y_train, X_validate, y_validate, X_test, y_test = train.iloc[:, 1:-1], train.iloc[:, -1], \
-                                validate.iloc[:, 1:-1], validate.iloc[:, -1], test.iloc[:, 1:-1], test.iloc[:, -1]
+                                                           validate.iloc[:, 1:-1], validate.iloc[:, -1], test.iloc[:,
+                                                                                                         1:-1], test.iloc[
+                                                                                                                :, -1]
+
 
 class Net(nn.Module):
-    def __init__(self, n_input,n_hidden,n_output):
+    def __init__(self, n_input, n_hidden, n_output):
         super(Net, self).__init__()
-        self.hidden1 = nn.Linear(n_input,n_hidden)
-        self.hidden2 = nn.Linear(n_hidden,n_hidden)
-        self.predict = nn.Linear(n_hidden,n_output)
-    def forward(self):
-        out = self.hidden1(input)
+        self.hidden1 = nn.Linear(n_input, n_hidden)
+        self.hidden2 = nn.Linear(n_hidden, n_hidden)
+        self.predict = nn.Linear(n_hidden, n_output)
+
+    def forward(self, data):
+        out = self.hidden1(data)
         out = F.relu(out)
         out = self.hidden2(out)
         out = F.relu(out)
-        out =self.predict(out)
+        out = self.predict(out)
         return out
 
-net = Net(1,20,1)
+
+net = Net(1, 20, 1)
 print(net)
 
-optimizer = torch.optim.SGD(net.parameters(),lr = 0.1)
+optimizer = torch.optim.SGD(net.parameters(), lr=0.1)
 loss_func = torch.nn.MSELoss()
