01589a21ac58c09b358ae034c6bed3894873d0cf
xuyou1999 <yx1711@nyu.edu>
2020-12-13 14:30:10 +0800

    nn

--- model/nn.py
+++ model/nn.py
@@ -16,3 +16,23 @@ test = pd.read_csv("../data/test_data.csv")
 
 X_train, y_train, X_validate, y_validate, X_test, y_test = train.iloc[:, 1:-1], train.iloc[:, -1], \
                                 validate.iloc[:, 1:-1], validate.iloc[:, -1], test.iloc[:, 1:-1], test.iloc[:, -1]
+
+class Net(nn.Module):
+    def __init__(self, n_input,n_hidden,n_output):
+        super(Net, self).__init__()
+        self.hidden1 = nn.Linear(n_input,n_hidden)
+        self.hidden2 = nn.Linear(n_hidden,n_hidden)
+        self.predict = nn.Linear(n_hidden,n_output)
+    def forward(self):
+        out = self.hidden1(input)
+        out = F.relu(out)
+        out = self.hidden2(out)
+        out = F.relu(out)
+        out =self.predict(out)
+        return out
+
+net = Net(1,20,1)
+print(net)
+
+optimizer = torch.optim.SGD(net.parameters(),lr = 0.1)
+loss_func = torch.nn.MSELoss()
