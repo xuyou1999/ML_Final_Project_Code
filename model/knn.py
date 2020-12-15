3d53e4470e7f1f5ecc125278e7e5df2d21c7731a
JunchengDong0421 <jd4115@nyu.edu>
2020-12-15 21:46:54 +0800

    small modifications

--- model/knn.py
+++ model/knn.py
@@ -90,24 +90,7 @@ test_score = knn.score(X_test, pred)
 print("Test MSE: %f" % test_MSE)
 print("Test Score: %f" % test_score)
 
-'''
 # plot
-pred_arg = pred.argsort()
-pred.sort()
-truth = y_test.values
-truth = [truth[arg] for arg in pred_arg]
-N = [i for i in range(len(y_test))]
-plt.plot(N, pred, 'r')
-plt.scatter(N, truth, s=1)
-plt.title("Model: KNN")
-plt.xlabel("movie")
-plt.ylabel("rating")
-plt.legend(("prediction", "ground truth"))
-plt.savefig("knn.png", dpi=420)
-plt.show()
-'''
-
-# plot 2
 truth = y_test.values
 truth_arg = truth.argsort()
 truth.sort()
--- model/nn.py
+++ model/nn.py
@@ -4,11 +4,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-from torch.autograd import Variable
 
 abs_path = os.path.abspath(__file__)
 path = os.path.dirname(abs_path)
 os.chdir(path)
+
+device = torch.device("cpu")
 
 train = pd.read_csv("../data/train_data.csv")
 validate = pd.read_csv("../data/validate_data.csv")
@@ -21,12 +22,14 @@ X_train, y_train, X_validate, y_validate, X_test, y_test = train.iloc[:, 1:-1], 
 
 X_train_tensor = torch.tensor(X_train.values).float()
 y_train_tensor = torch.tensor(y_train.values).float()
-y_train_tensor = torch.reshape(y_train_tensor, (y_train.shape[0],1))
+y_train_tensor = torch.reshape(y_train_tensor, (y_train.shape[0], 1))
 
+batch_size = len(X_train)
 num_features = 7
 num_output = 1
+num_hidden = 10
 # dimensions
-B, D_in, H, D_out = len(X_train), num_features, 3, num_output
+B, D_in, H, D_out = batch_size, num_features, num_hidden, num_output
 m = torch.nn.BatchNorm1d(num_features, affine=True)
 n = torch.nn.BatchNorm1d(num_output, affine=True)
 X_train_tensor = m(X_train_tensor)
@@ -34,6 +37,7 @@ y_train_tensor = n(y_train_tensor)
 
 print(X_train_tensor)
 print(y_train_tensor)
+
 
 class Net(nn.Module):
     def __init__(self, n_input, n_hidden, n_output):
@@ -51,13 +55,12 @@ class Net(nn.Module):
         return out
 
 
-net = Net(7, 4, 1)
-print(net)
-
-optimizer = torch.optim.SGD(net.parameters(), lr=0.05)
+epoch, learning_rate = 5000, 0.05
+net = Net(D_in, H, D_out)
+optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate)
 loss_func = torch.nn.MSELoss()
 
-for t in range(5000):
+for t in range(epoch):
     prediction = net(X_train_tensor)
     loss = loss_func(prediction, y_train_tensor)
 
@@ -65,7 +68,7 @@ for t in range(5000):
     loss.backward(retain_graph=True)
     optimizer.step()
 
-    if t%5 ==0:
+    if t % 5 == 0:  # plot every 5 epochs
         plt.cla()
         plt.scatter(X_train_tensor[:, 1].detach().numpy(), y_train_tensor.detach().numpy())
         plt.scatter(X_train_tensor[:, 1].detach().numpy(), prediction.data.numpy())
@@ -77,11 +80,11 @@ plt.show()
 
 X_test_tensor = torch.tensor(X_test.values).float()
 y_test_tensor = torch.tensor(y_test.values).float()
-y_test_tensor = torch.reshape(y_test_tensor, (y_test.shape[0],1))
+y_test_tensor = torch.reshape(y_test_tensor, (y_test.shape[0], 1))
 
 X_test_tensor = m(X_test_tensor)
 y_test_tensor = n(y_test_tensor)
 
 pred = net(X_test_tensor)
 print(pred)
-print(loss_func(pred, y_test_tensor)) # 0.0646
+print(loss_func(pred, y_test_tensor))  # 0.0623
