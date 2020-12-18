978d4c34de987823b5450f7bc180887a31beb0c2
JunchengDong0421 <jd4115@nyu.edu>
2020-12-18 20:28:54 +0800

    delete old data

--- old_data/extract_id.py
+++
@@ -1,8 +0,0 @@
-from DoubanAPI import DoubanAPI
-
-
-if __name__ == '__main__':
-    data_size = 3000
-    out = "movie.txt"
-    with DoubanAPI() as D:
-        D.find_id(False, "电影", 0, (2010, 2019), out, data_size)
